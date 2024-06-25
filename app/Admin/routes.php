222f3be65cb513a9192673b342d67d7b248d8987
superod <supgeek.rod@gmail.com>
2024-06-25 15:47:07 +0800

    chore: 恢复原有的 laravel-admin

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,8 +10,16 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
     'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-
     $router->get('/', 'HomeController@index')->name('home');
 
+    $router->resource('users', 'UserController');
+    $router->resource('posts', 'PostController');
+    $router->resource('comments', 'CommentController');
+    $router->resource('thumbs', 'ThumbController');
+    $router->resource('notices', 'NoticeController');
+    $router->resource('user-reports', 'UserReportController');
+
     $router->resource('activities', 'ActivityController');
+
+    $router->get('system', 'SystemController@index')->name('admin.system');
 });
